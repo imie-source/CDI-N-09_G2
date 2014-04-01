a9ea583ebc1271e5a4fe226d18bb4139592c1556
Locron <rethoreronan@free.fr>
2014-04-01 15:21:57 +0200

    conflit

--- testProject/src/HelloWorld.java
+++ testProject/src/HelloWorld.java
@@ -1,14 +1,14 @@
-
-public class HelloWorld {
+public class HelloWorld
+{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) 
+
+	public static void main(String[] args)
 	{
 		System.out.println("Hello World ! Je suis moi");
 		System.out.println("Bonjour moi.");
 		System.out.println("test contrib.");
 	}
-
 }
