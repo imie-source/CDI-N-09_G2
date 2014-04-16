8b117b980f3d14cf00223803ded53aec993ebbfe
kmegaoui <kevin.megaoui@gmail.com>
2014-04-16 16:39:41 +0200

    ajout Entite Promotion et formatter

--- EntitesJPA/src/model/Promotion.java
+++ EntitesJPA/src/model/Promotion.java
@@ -1,26 +1,49 @@
+
 package model;
 
 import java.io.Serializable;
-import javax.persistence.*;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * Entity implementation class for Entity: Promotion
- *
+ * 
  */
 @Entity
-
-public class Promotion implements Serializable 
+public class Promotion implements Serializable
 {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="prm_id", unique=true, nullable=false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "prm_id", unique = true, nullable = false)
 	private Integer id;
-	
-	public Promotion() {
+
+	@Column(name = "prm_intitule", length = 50)
+	private String intitule;
+
+	@Column(name = "prm_lieu", length = 25)
+	private String lieu;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "prm_datedebut")
+	private Date dateDebut;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "prm_datefin")
+	private Date dateFin;
+
+	public Promotion()
+	{
 		super();
 	}
-	
+
 }
