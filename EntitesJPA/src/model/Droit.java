d2ae6e15a35cf25acbc45ec6a575a86dd0964c9d
Locron <rethoreronan@free.fr>
2014-04-18 14:40:09 +0200

    Ajout entity Droit

---
+++ EntitesJPA/src/model/Droit.java
@@ -0,0 +1,40 @@
+
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity implementation class for Entity: Droit
+ * 
+ */
+@Entity
+@Table(name = "droit")
+public class Droit implements Serializable
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2849769808683088879L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "droit_id")
+	private Integer id;
+
+	@Column(name = "droit_intitule", nullable = false, length = 25)
+	private String intitule;
+
+	public Droit()
+	{
+		super();
+	}
+
+}
